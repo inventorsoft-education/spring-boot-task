fbbb0d1a4453da18cd854194567163bde00b3089
Alex <paskar.olexandr@gmail.com>
2020-10-09 17:33:30 +0300

    Created all required folders to run the project

---
+++ src/main/java/com/paskar/email/application/EmailDeliveryApplication.java
@@ -0,0 +1,12 @@
+package com.paskar.email.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailDeliveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmailDeliveryApplication.class, args);
+    }
+
+}
