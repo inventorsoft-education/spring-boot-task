a538221725e1215478f335375e31320f7cda8331
Alex <paskar.olexandr@gmail.com>
2020-10-12 16:46:50 +0300

    superfluous imports have been deleted

--- src/main/java/com/paskar/email/application/EmailDeliveryApplication.java
+++ src/main/java/com/paskar/email/application/EmailDeliveryApplication.java
@@ -1,10 +1,7 @@
 package com.paskar.email.application;
 
 import com.paskar.email.application.console.Email;
-import com.paskar.email.application.console.InformationFromConsole;
 import com.paskar.email.application.repositiory.CreateAndSaveEmail;
-import com.paskar.email.application.repositiory.FindEmail;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.paskar.email.application.console.InformationFromConsole.*;
+import static com.paskar.email.application.console.InformationFromConsole.numberOfLettersValidation;
 
 @SpringBootApplication
 @EnableScheduling
