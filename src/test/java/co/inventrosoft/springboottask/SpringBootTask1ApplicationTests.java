c1d432f94d6ee16a8d5bc5b2df151350e0bb1f5f
Stas Kachurovskiy <larevoo@gmail.com>
2021-06-28 19:12:50 +0300

    spring boot initialized

---
+++ src/test/java/co/inventrosoft/springboottask/SpringBootTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.inventrosoft.springboottask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTask1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
