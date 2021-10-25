863fa192192a893c03fe0f4fb1a39fbc15db7420
tsiupa-coder <ivan.tsiupa111@gmail.com>
2021-10-25 10:31:53 +0300

    class console

---
+++ src/main/java/com/example/task5/service/Console.java
@@ -0,0 +1,26 @@
+package com.example.task5.service;
+
+import com.example.task5.repository.ListOfGames;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class Console {
+
+    public<T> void printList(String message, List<T> list){
+        System.out.println(message);
+        for ( T t: list) {
+            System.out.println(t);
+        }
+    }
+    
+    public <T> void printMap(String message, Map<String, List<T>> map){
+        System.out.println(message);
+        for (String s : map.keySet()) {
+            map.get(s).stream().forEach(System.out::println);
+        }
+    }
+
+}
