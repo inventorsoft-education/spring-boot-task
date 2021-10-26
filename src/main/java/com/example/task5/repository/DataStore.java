50de4dbb532095e7bf43dd1752f1f7bfef65becb
tsiupa-coder <ivan.tsiupa111@gmail.com>
2021-10-26 13:53:32 +0300

    interface is now abstract

--- src/main/java/com/example/task5/repository/DataStore.java
+++ src/main/java/com/example/task5/repository/DataStore.java
@@ -2,23 +2,16 @@ package com.example.task5.repository;
 
 import com.example.task5.model.Game;
 import com.example.task5.model.Team;
-import com.example.task5.service.CSV;
-import org.springframework.beans.factory.annotation.Value;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public interface DataStore {
 
-    default ArrayList<Team> getData(ArrayList<Team> teams) {
-
-        return  new CSV().read(teams, "file1.csv");
-
-    }
-
-    default HashMap<String, List<Game>> getData(HashMap<String, List<Game>> games){
-        return new CSV().read(games, "file2.csv");
-    }
+    ArrayList<Team> getData(ArrayList<Team> team);
+    HashMap<String, List<Game>> getData(HashMap<String, List<Game>> games);
+    void setData(ArrayList<Team> list);
+    void setData(Map<String, List<Game>> map);
 
 }
