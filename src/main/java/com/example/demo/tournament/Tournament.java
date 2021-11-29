a8e04b0a4a7d9ababa8a1d0bfe10c53c0515f597
Vlad Zhloba <zhloba96@gmail.com>
2021-11-29 13:02:09 +0200

    the redesigned class

--- src/main/java/com/example/demo/tournament/Tournament.java
+++ src/main/java/com/example/demo/tournament/Tournament.java
@@ -2,69 +2,75 @@ package com.example.demo.tournament;
 
 import com.example.demo.model.Match;
 import com.example.demo.model.Team;
-import com.example.demo.repository.BaseMatchRepository;
-import com.example.demo.repository.BaseTeamRepository;
-import com.example.demo.repository.TeamRepository;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+import com.example.demo.dao.MatchDAO;
+import com.example.demo.dao.TeamDAO;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
 import java.rmi.UnexpectedException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.function.Supplier;
 
 @Component
 @Value
 @AllArgsConstructor
 public class Tournament {
-    BaseTeamRepository teams;
-    BaseMatchRepository matches;
 
-    public void createMatchesBasedOnListOfTeams() {
-        List<Team> teamList = teams.getList();
-        if (teams.checkValidCntOfTeams()) {
-            Random random = new Random();
-            int firstRandomNumber;
-            int secondRandomNumber;
-            int countMatchesAtTheRound = teamList.size() / 2;
-            int sizeOfIndexArray = teamList.size();
-            int cntOfMatches = teamList.size() - 1;
+    public boolean createAndSaveMatchesBasedOnListOfTeams(List<Team> teams) {
+        List<Match> matches = new ArrayList<>();
+
+        if (teams.size() % 2 == 0) {
+            Team firstTmpTeam;
+            Team secondTmpTeam;
+            int firstRandomValue;
+            int secondRandomValue;
+            int countMatchesAtTheRound = teams.size() / 2;
+            int cntOfMatches = teams.size() - 1;
             int currentRound;
             int cntOfRounds;
-            List<Integer> indexes = generateIndexList(sizeOfIndexArray);
-
+            List<Integer> indexes = generateIndexList(teams.size());
+            List<Integer> tmpIndexes;
             cntOfRounds = returnCntOfRounds(cntOfMatches);
 
             for (int i = 1; i <= cntOfRounds; i++) {
                 currentRound = i;
+                tmpIndexes = new ArrayList<>();
                 for (int j = 0; j < countMatchesAtTheRound; j++) {
-                    firstRandomNumber = random.nextInt(sizeOfIndexArray);
-                    while (!indexes.contains(firstRandomNumber)) {
-                        firstRandomNumber = random.nextInt(sizeOfIndexArray);
+                    firstRandomValue = getRandomValueFromArray(indexes);
+                    indexes.remove((Object) firstRandomValue);
+                    secondRandomValue = getRandomValueFromArray(indexes);
+                    indexes.remove((Object) secondRandomValue);
+
+                    firstTmpTeam = teams.get(firstRandomValue);
+                    secondTmpTeam = teams.get(secondRandomValue);
+
+                    Match match = new Match(firstTmpTeam.getName(), secondTmpTeam.getName(), currentRound, cntOfRounds, Match.generateScore());
+                    matches.add(match);
+
+                    if (getWinner(match).getName().equals(firstTmpTeam.getName())) {
+                        tmpIndexes.add(firstRandomValue);
                     }
-                    indexes.remove((Object) firstRandomNumber);
-                    secondRandomNumber = random.nextInt(sizeOfIndexArray);
-                    while (!indexes.contains(secondRandomNumber)) {
-                        secondRandomNumber = random.nextInt(sizeOfIndexArray);
-                    }
-                    indexes.remove((Object) secondRandomNumber);
-                    matches.getList().add(new Match(teamList.get(firstRandomNumber), teamList.get(secondRandomNumber), currentRound, cntOfRounds, Match.generateScore()));
+                    else tmpIndexes.add(secondRandomValue);
                 }
-                sizeOfIndexArray = matches.getList().size();
-                indexes = generateIndexList(sizeOfIndexArray);
+                indexes = new ArrayList<>(tmpIndexes);
                 countMatchesAtTheRound /= 2;
             }
         }
+        MatchDAO.save(matches);
+        return true;
+    }
 
+    private Integer getRandomValueFromArray(List<Integer> indexes) {
+        if(indexes.size() == 1) return indexes.get(indexes.size()-1);
+
+        Random random = new Random();
+        Integer tmp = random.nextInt(indexes.get(indexes.size()-1));
+        while (!indexes.contains(tmp)) {
+            tmp = random.nextInt(indexes.get(indexes.size()-1));
+        }
+        return tmp;
     }
 
     private Integer returnCntOfRounds(int cntOfMatches) {
@@ -85,27 +91,14 @@ public class Tournament {
         return arr;
     }
 
-    public List<Team> getWinner() {
-        try {
-            return new ArrayList<Team>(List.of(matches.getList().get(matches.getList().size() - 1).getWinner()));
-        } catch (UnexpectedException e) {
-            System.out.println("No winner, draw");
-            return new ArrayList<Team>(List.of(
-                    matches.getList().get(matches.getList().size() - 1).getFirstTeam(),
-                    matches.getList().get(matches.getList().size() - 1).getSecondTeam()
-            ));
-        }
+    public Team getWinner() {
+        List<Match> matches = MatchDAO.getList();
+        Team winner = TeamDAO.getTeamByName(matches.get(matches.size() - 1).getWinnerName());
+        return winner;
     }
 
-    public void exportWinner() {
-        String path = "src/main/resources/winner.csv";
-
-        try (Writer writer = new FileWriter(path)) {
-            StatefulBeanToCsv sbc = new StatefulBeanToCsvBuilder(writer)
-                    .build();
-            sbc.write(getWinner());
-        } catch (IOException | CsvDataTypeMismatchException | CsvRequiredFieldEmptyException e) {
-            e.printStackTrace();
-        }
+    public Team getWinner(Match match) {
+        Team winner = TeamDAO.getTeamByName(match.getWinnerName());
+        return winner;
     }
 }
