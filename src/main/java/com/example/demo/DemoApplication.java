960a7171f6bca82927d3ea0570fe3de62c9a41f6
Vlad Zhloba <zhloba96@gmail.com>
2021-11-29 13:00:04 +0200

    demo run changed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
 import com.example.demo.console.ConsoleInOut;
+import com.example.demo.dao.TeamDAO;
+import com.example.demo.model.Team;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 @AllArgsConstructor
@@ -18,7 +22,18 @@ public class DemoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        console.getTournament().createMatchesBasedOnListOfTeams();
+        TeamDAO.save(List.of(
+                new Team("name1","capitan1","coach1"),
+                new Team("name2","capitan2","coach2"),
+                new Team("name3","capitan3","coach3"),
+                new Team("name4","capitan4","coach4"),
+                new Team("name5","capitan5","coach5"),
+                new Team("name6","capitan6","coach6"),
+                new Team("name7","capitan7","coach7"),
+                new Team("name8","capitan8","coach8")
+                ));
+
+        console.getTournament().createAndSaveMatchesBasedOnListOfTeams(TeamDAO.getList());
         console.outMatchList();
         console.outWinner();
     }
